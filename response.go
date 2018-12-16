843d1c8db4102f5fe3ccb0a85e11ea030621e18c
Tyler Smith <tylersmith.me@gmail.com>
2018-12-15 18:19:52 -0800

    Add a bunch of godoc comments.

--- response.go
+++ response.go
@@ -24,23 +24,28 @@ func (r Response) GetRound() int64 {
 	return r.round
 }
 
+// RequestRecord is a poll request for more votes
 type RequestRecord struct {
 	timestamp int64
 	invs      []Inv
 }
 
+// NewRequestRecord creates a new RequestRecord
 func NewRequestRecord(timestamp int64, invs []Inv) RequestRecord {
 	return RequestRecord{timestamp, invs}
 }
 
+// GetTimestamp returns the timestamp that the request was created
 func (r RequestRecord) GetTimestamp() int64 {
 	return r.timestamp
 }
 
+// GetInvs returns the poll Invs for the request
 func (r RequestRecord) GetInvs() []Inv {
 	return r.invs
 }
 
+// IsExpired returns true if the request has expired
 func (r RequestRecord) IsExpired() bool {
 	return time.Unix(r.timestamp, 0).Add(AvalancheRequestTimeout).Before(clock.Now())
 }
